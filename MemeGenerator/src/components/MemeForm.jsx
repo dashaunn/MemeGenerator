c63a3f7711f021b58349afed41732109ec8fdbee
Dashaun Sutton-Harris <114096734+dashaunn@users.noreply.github.com>
2024-01-31 23:42:02 -0600

    update MemeForm component

--- MemeGenerator/src/components/MemeForm.jsx
+++ MemeGenerator/src/components/MemeForm.jsx
@@ -2,19 +2,31 @@ import React from 'react'
 
 export default function MemeForm() {
     return (
-        <form className="meme">
-            <div className='input-container'>
-                <div className='input-group'>
-                    <h4>Top Text</h4>
-                    <input className="input" type='text'></input>
+        <main>
+            <form className="form">
+
+                <div>
+                    <label className='form--label'>Top Text</label>
+                    <input
+                        className="form--input" 
+                        type='text'
+                        placeholder='That moment when'
+                    />
                 </div>
-                <div className='input-group'>
-                    <h4>Bottom text</h4>
-                    <input className="input" type='text'></input>
+                
+
+                <div>
+                    <label className='form--label'>Bottom Text</label>
+                    <input
+                        className="form--input"
+                        type='text'
+                        placeholder='you burned your toast'
+                    />
                 </div>
-            </div>
-            
-            <button className="form-submit" type='submit'>Get a new meme image  🖼</button>
-        </form>
+                
+
+                <button className="form--button" type='submit'>Get a new meme image  🖼</button>
+            </form>
+        </main>
     )
 }
